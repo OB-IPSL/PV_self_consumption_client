85a5f68f7a37b047de6502a88681d6060d851c21
Olivier BOUCHER <olivier.boucher@ipsl.fr>
2024-10-10 20:48:02 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
 # PV_self_consumption_api
+
+Authors: 
+Sébastien Gardoll, CNRS, IPSL
+Olivier Boucher, CNRS, IPSL
+
+(c) 2024 
+
+Interface to call API PV_self_consumption_api for optimising self consumption under constraints.
+Note that the license only applies to this interface and not to the api
+
+## Python environment
+
+### Option 1: Conda
+###----------------
+
+1. Install miniconda;
+2. Create a new conda environment;
+3. Install the project dependencies.
+
+Skip 1. if you already have a miniconda/anaconda distribution installed (`which conda` doesn't return an error).
+
+#### Install miniconda
+
+While installing, Miniconda asks you to initialize itself. If you choose to do so, it will add some instructions to your ~/.bashrc (shell configuration).
+Carefully choose the path of the Miniconda installation directory, as conda environment take some space and inodes. The following example is meant for Linux x86_64.
+
+```bash
+curl -sSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh > Miniconda3-latest-Linux-x86_64.sh
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh
+```
+
+#### Conda environment creation
+
+Let's create a conda environment called pvsc:
+
+```bash
+conda create -y -n pvsc 'python=3.12.*'
+```
+
+#### Project dependencies installation
+
+First activate the environment (as usual), then install the dependencies with pip:
+
+```bash
+conda activate pvsc
+cd /path/to/PV_self-consumption/
+pip install -e .
+```
+
+### Option 2: PDM
+###--------------
+
+[PDM](https://pdm-project.org/en/latest/) associates a Python virtual environmnent (venv) with a project described by a pyproject file. 
+
+#### Dependencies installation
+
+Setup the project environment with PDM:
+
+```bash
+cd /path/to/PV_self-consumption/
+pdm install
+```
+
+#### Environement activation
+
+Activate the associated venv:
+
+```bash
+cd /path/to/PV_self-consumption/
+eval $(pdm venv activate)
+```
+
+Deactivate as usual:
+
+```bash
+deactivate
+```
---
+++ client_api.py
@@ -0,0 +1,76 @@
+import requests
+import numpy as np
+from numpy.typing import NDArray
+from pydantic import BaseModel, ConfigDict
+from pathlib import Path
+from typing import Any
+from pydantic.functional_validators import field_validator
+
+
+API_URL = 'http://voc-dev.ipsl.fr:9999/api/optimisesc'
+
+
+class Parameters(BaseModel):
+    model_config = ConfigDict(extra='ignore')
+    supply: list[float]
+    price_sale: float
+    price_buy: float
+    Emax: float
+    Imax: float
+    Bmax: float
+    ts_in: float
+    ts_out: float
+    Beff: float
+    B0f: float
+    dB: float
+    Nscen: int
+    dt: float
+
+    @field_validator('supply')
+    @classmethod
+    def convert_ndarray_to_list(cls, value):
+        if isinstance(value, np.ndarray):
+            return np.array(value)
+        else:
+            return value
+
+
+class Result:
+    Cusage: dict[str, NDArray]
+    P: NDArray
+    C: NDArray
+    Enet: NDArray
+    Curt: NDArray
+    Bnet: NDArray
+    Bstates: NDArray
+    L: NDArray
+    
+    def __init__(self, raw_result: dict[str, list[float]|dict[str, list[float]]]) -> None:
+        for key, value in raw_result.items():
+            setattr(self, key, value)
+
+    @staticmethod
+    def get_list_class_members():
+        return vars(Result)['__annotations__'].keys()
+
+def create_parameters(script_variables: dict[Any, Any]) -> Parameters:
+    return Parameters(**script_variables)
+
+
+def numpy_array_decoder(object: dict):
+        for key, value in object.items():
+            if isinstance(value, list):
+                object[key] = np.array(value)
+            elif isinstance(value, dict):
+                for k, v in value.items():
+                    value[k] = np.array(value[k])
+                object[key] = value
+        return object   
+
+
+def optimize_sc_stub(parameters: Parameters, demand_file_path: Path) -> Result:
+    with open(demand_file_path, 'rb') as demand_file:
+        files = {'demand_file': demand_file}
+        r = requests.post(API_URL, data={'data': parameters.model_dump_json()}, files=files)
+        result = Result(r.json(object_hook=numpy_array_decoder))
+        return result
