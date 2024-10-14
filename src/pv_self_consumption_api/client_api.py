7fe0fbf1b9b8d6e1f7509306d383c86938db5dcc
Sébastien Gardoll <sebastien@gardoll.fr>
2024-10-14 16:14:19 +0200

    update

--- src/pv_self_consumption_api/client_api.py
+++ src/pv_self_consumption_api/client_api.py
@@ -5,10 +5,13 @@ from pydantic import BaseModel, ConfigDict
 from pathlib import Path
 from typing import Any
 from pydantic.functional_validators import field_validator
+import yaml
+import pandas as pd
+import pv_self_consumption_api.utils as utils
 
-
-API_URL = 'http://localhost:9999/api/optimisesc'
-
+DEFAULT_API_HOST = 'klima.ipsl.fr'
+DEFAULT_API_PORT = 443
+API_ROUTE = '/api/pv_self_consumption/optimisesc'
 
 class Parameters(BaseModel):
     model_config = ConfigDict(extra='ignore')
@@ -52,9 +55,9 @@ class Result:
     @staticmethod
     def get_list_class_members():
         return vars(Result)['__annotations__'].keys()
-
-def create_parameters(script_variables: dict[Any, Any]) -> Parameters:
-    return Parameters(**script_variables)
+    
+    def __str__(self) -> str:
+        return super.__str__(self) # TODO
 
 
 def numpy_array_decoder(object: dict):
@@ -68,9 +71,35 @@ def numpy_array_decoder(object: dict):
         return object   
 
 
-def optimize_sc_stub(parameters: Parameters, demand_file_path: Path) -> Result:
+def _check_files(parameter_file_path: Path, demand_file_path: Path)-> Parameters:
+    
+    if not parameter_file_path.exists():
+        raise Exception('missing parameter file')
+    if not demand_file_path.exists():
+        raise Exception('missing demand file')
+    with open(parameter_file_path, 'r') as parameter_file:
+        try:
+            dict_parameters = yaml.safe_load(parameter_file)
+            result = Parameters(**dict_parameters)
+        except Exception as e:
+            raise Exception(f'unable to parse parameters: {str(e)}')
+        try:
+            demand = pd.read_csv(demand_file_path, skiprows=12, skipinitialspace=True)
+            demand.set_index('usage', inplace=True)
+        except Exception as e:
+            raise Exception(f'unable to read demand file: {str(e)}')
+        try:
+            utils.check_compliance_inputs(demand=demand, **dict_parameters)
+        except Exception as e:
+            raise Exception(f'wrong parameters or demand: {str(e)}')
+        return result
+
+
+def optimize_sc(parameter_file_path: Path, demand_file_path: Path,
+                port: int = DEFAULT_API_PORT, host: str = DEFAULT_API_HOST) -> tuple[Result, Parameters]:
+    parameters = _check_files(parameter_file_path, demand_file_path)
     with open(demand_file_path, 'rb') as demand_file:
         files = {'demand_file': demand_file}
-        r = requests.post(API_URL, data={'data': parameters.model_dump_json()}, files=files)
+        r = requests.post(f'http://{host}:{port}/{API_ROUTE}', data={'data': parameters.model_dump_json()}, files=files)
         result = Result(r.json(object_hook=numpy_array_decoder))
-        return result
+        return result, parameters
--- src/pv_self_consumption_api/utils.py
+++ src/pv_self_consumption_api/utils.py
@@ -1,16 +1,81 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-#--make plots from output of optimisation
+#--check the compliance of the inputs and parameters
+def check_compliance_inputs(supply,demand,price_sale,price_buy,Emax,Imax,Bmax,ts_in,ts_out,Beff,B0f,dB,Nscen,dt):
+    #
+    eps=1.e-6
+    #
+    assert price_sale < price_buy, 'price_sale has to be smaller than price_buy to maximize self-consumption'
+    #
+    assert dt > 0, f'dt has to be strictly positive ({dt})'
+    assert abs(1/dt - int(1/dt)) < eps, f'dt should be a fraction of an hour {dt}'
+    #
+    assert dB > 0, f'dB has to be strictly positive ({dB})'
+    assert Emax >= 0, f'Emax has to be positive ({Emax})'
+    assert Imax >= 0, f'Imax has to be positive ({Imax})'
+    assert Bmax > 0, f'Bmax has to be strictly positive ({Bmax})'
+    assert ts_in > 0, f'ts_in has to be strictly positive ({ts_in})'
+    assert ts_out > 0, f'ts_out has to be strictly positive ({ts_out})'
+    assert Beff > 0, f'Beff has to be strictly positive ({Beff})'
+    assert Beff <= 1, f'Beff has to be less than unity ({Beff})'
+    assert B0f >= 0, f'B0f has to be positive ({B0f})'
+    assert B0f <= 1, f'B0f has to be less than 1 ({B0f})'
+    assert Nscen >= 100, f'Nscen has to be larger than 100 ({Nscen})'
+    assert Nscen <= 10000, f'Nscen has to be less than 10000 ({Nscen})'
+    #
+    assert (demand.L).any() >=0, 'all of the L values have to be positive'
+    assert (demand.P).any() >=0, 'all of the P values have to be positive'
+    assert (demand.E).any() >=0, 'all of the E values have to be positive'
+    assert (demand.Pmax).any() >=0, 'all of the Pmax values have to be positive'
+    #
+    for usage,row in demand.iterrows():
+        L=row.L
+        P=row.P
+        E=row.E
+        Pmax=row.Pmax
+        i1=row.i1
+        i2=row.i2
+        uniform=row.uniform
+        intermittent=row.intermittent
+        assert i1 >= 0, f'i1 for usage {usage} has to be larger or equal to 0 ({i1})'
+        assert i2 <= len(supply), f'i2 for usage {usage} has to be smaller or equal to the size of supply {i2}'
+        assert i2 > i1, f'i2 for usage {usage} has to be strictly larger than i1 ({i2} <= {i1})'
+        assert i2-i1>=L, f'i2-i1 for usage {usage} has to be larger or equal to L ({i2-i1} < {L})'
+        assert (uniform==True) | (intermittent==True), f'uniform and intermittent cannot be both False for {usage}'
+        if uniform:
+           assert L >= 1, f'L for uniform usage {usage} has to be larger or equal to 1 ({L})'
+           assert P >= 1, f'P for uniform usage {usage} has to be larger or equal to 1 ({P})'
+           assert E == 0, f'E for uniform usage {usage} has to be equal to 0 ({E})'
+           assert Pmax == 0, f'Pmax for uniform usage {usage} has to be equal to 0 ({Pmax})'
+        else:
+           assert L == 0, f'L for non-uniform usage {usage} has to be equal to 0 ({L})'
+           assert P == 0, f'P for non-uniform usage {usage} has to be equal to 0 ({P})'
+           assert E >= 1, f'E for non-uniform usage {usage} has to be larger or equal to 1 ({E})'
+           assert Pmax >= 1, f'Pmax for non-uniform usage {usage} has to be larger or equal to 1 ({Pmax})'
+           assert (i2-i1)*Pmax >= E, f'Pmax for non-uniform usage {usage} has to be large enough to allow completion of E ({i1=}, {i2=}, {Pmax=}, {E=})'
+    #
+    return
+
+#--make plots
 def make_figure(price_buy,price_sale,demand,Cusage,P,C,Enet,Emax,Imax,Curt,L,Bnet,Bstates,Ntimes,pltshow,dirplots):
     #
     eps=1e-6
     #
+    #--convert uniform prices to numpy array
+    if isinstance(price_buy,(int,float)):
+        price_buy=np.ones((Ntimes))*price_buy
+    if isinstance(price_sale,(int,float)):
+        price_sale=np.ones((Ntimes))*price_sale
+    #
+    #--define figure
     fig, axs = plt.subplots(5,figsize=(6,8))
     #
-    axs[0].stairs([price_sale]*Ntimes,label='Sale')
-    axs[0].stairs([price_buy]*Ntimes,label='Buy')
+    axs[0].stairs(price_sale,label='Sale')
+    axs[0].stairs(price_buy,label='Buy')
     axs[0].set_ylabel('Price')
     axs[0].set_xlim(0,Ntimes+eps)
+    axs[0].set_ylim(0,0.3)
     axs[0].set_xticks(range(0,Ntimes))
     axs[0].set_xticklabels([])
     axs[0].set_title('Electricity price (in €/kWh)')
